5d7ab8cefb2ed5e8b7050a1ce8ee16d41607c9fe
MikeCornerstone1199955 <125836756+MikeCornerstone1199955@users.noreply.github.com>
2024-02-08 05:29:56 -0800

    Add files via upload

---
+++ mike.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // question 1
+        $Number = 100;
+        $Num = 100.5;
+        $Name = 'Micheal';
+        
+// question 2
+        $stdin = fopen("enter the first number", 'Num')
+
+        $i = 20;
+        $r = 30;
+        $Name = 'adeyemi';
+        $sum = $i + $r;
+        $concat_string = $sum .$Name; 
+
+        echo $sum . '\n';
+
+
+
+
+
+
+
+
+        
+
+
+
+    ?>
+</body>
+</html>
